fbee8762770e7518f1e04f0fc501f86c52ea0d57
vdbobila <euanbobila@gmail.com>
2026-08-24 02:39:21 +0800

    Add files via upload

---
+++ q1/q1_sg2_a3.py
@@ -0,0 +1,9 @@
+chnzodiacs = ["Rat (鼠 / Shǔ)", "Ox (牛 / Niú)", "Tiger (虎 / Hǔ)", "Rabbit (兔 / Tù)", "Dragon (龙 / Lóng)", "Snake (蛇 / Shé)", "Horse (马 / Mǎ)", "Goat (羊 / Yáng)", "Monkey (猴 / Hóu)", "Rooster (鸡 / Jī)", "Dog (狗 / Gǒu)", "Pig (猪 / Zhū)"]
+birthyear = int(input("Enter your birth year: "))
+
+if birthyear < 1900:
+    print("Invalid year, it should not be earlier than 1900")
+else:
+    zodiaccalc = (birthyear - 1900) % 12
+    urzodiac = chnzodiacs[zodiaccalc]
+    print(f"Your Chinese Zodiac Sign is : {urzodiac}")
